968aa9be55fde67901a247361c7498d8346f4bf7
Akshay Easwaran <akeaswaran@me.com>
2018-06-20 21:32:56 -0700

    cleaning up the site for public site display only, added custom error page

--- app.js
+++ app.js
@@ -7,36 +7,9 @@ var bodyParser = require('body-parser');
 
 require('dotenv').config();
 
-var firebase = require('firebase');
-var config = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN + ".firebaseapp.com",
-    databaseURL: "https://" + process.env.FIREBASE_DATABASE_URL +".firebaseio.com",
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET + ".appspot.com",
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
-};
-firebase.initializeApp(config);
-
-var firebaseAdmin = require('firebase-admin');
-var serviceAccount = require("./service-account-key.json");
-
-firebaseAdmin.initializeApp({
-    credential: firebaseAdmin.credential.cert(serviceAccount),
-    databaseURL: "https://" + process.env.FIREBASE_DATABASE_URL +".firebaseio.com",
-});
-
 var app = express();
 
 var index = require('./routes/index');
-var families = require('./routes/families')(firebase);
-var users = require('./routes/users')(firebase);
-var events = require('./routes/events')(firebase);
-var points = require('./routes/points')(firebase);
-var login = require('./routes/login')(firebase);
-
-var admin = require('./routes/admin')(firebaseAdmin, firebase);
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -57,12 +30,6 @@ app.use(session({
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/members', users);
-app.use('/events', events.router);
-app.use('/points', points);
-app.use('/families', families);
-app.use('/login', login);
-app.use('/admin', admin);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -79,7 +46,10 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', {
+      title: 'Error | Ramblin\' Reck Club',
+      moment: require('moment')
+  });
 });
 
 module.exports = app;
