3049a1bb0dfc4dada5064a8a1d9c9b4e07037ff8
Akshay Easwaran <akeaswaran@me.com>
2019-02-26 17:29:31 -0500

    removing more PHP about families 5 and 6

--- families.php
+++ families.php
@@ -18,6 +18,9 @@ if($_SESSION[isAdmin]==1 || $_SESSION[isVP]==1) {
      echo "<form action=\"editFamilies.php\" method=\"POST\">";
      echo "<tr><td><input type=\"submit\" value=\"Edit Families\"></td></tr>";
      echo "</form>";
+    echo "<form action=\"familyEvents.php\" method=\"POST\">";
+    echo "<tr><td><input type=\"submit\" value=\"Family Events\"></td></tr>";
+    echo "</form>";
      echo "</table>";
      echo "<br/><br/>";
 }
@@ -96,43 +99,6 @@ if($_SESSION[isAdmin]==1 || $_SESSION[isVP]==1) {
 		echo $row[memberPoints]."<br>";
 	}
 echo "</td></tr>";
-	echo "<tr bgcolor=\"#dbcfba\"><th>";
-	$query = $db->query("SELECT familyName FROM Family WHERE familyID=5");
-		$query->setFetchMode(PDO::FETCH_ASSOC);
-	$row = $query->fetch();
-	echo $row[familyName];
-	echo "</th><th>Points</th><th>";
-	$query = $db->query("SELECT familyName FROM Family WHERE familyID=6");
-		$query->setFetchMode(PDO::FETCH_ASSOC);
-	$row = $query->fetch();
-	echo $row[familyName];
-	echo "</th><th>Points</th></tr>";
-	echo "<tr><td>";
-	$query = $db->query("SELECT firstName, lastName, memberPoints FROM Member WHERE memFamilyID=5 AND status != 'alumni' ORDER BY memberPoints DESC, lastName, firstName");
-		$query->setFetchMode(PDO::FETCH_ASSOC);
-	while($row = $query->fetch()) {
-		echo $row[firstName]." ".$row[lastName]."<br>";
-	}
-	echo "</td><td>";
-	$query = $db->query("SELECT firstName, lastName, memberPoints FROM Member WHERE memFamilyID=5 AND status != 'alumni' ORDER BY memberPoints DESC, lastName, firstName");
-		$query->setFetchMode(PDO::FETCH_ASSOC);
-	while($row = $query->fetch()) {
-		echo $row[memberPoints]."<br>";
-	}
-	echo "</td><td>";
-	$query = $db->query("SELECT firstName, lastName, memberPoints FROM Member WHERE memFamilyID=6 AND status != 'alumni' ORDER BY memberPoints DESC, lastName, firstName");
-		$query->setFetchMode(PDO::FETCH_ASSOC);
-	while($row = $query->fetch()) {
-		echo $row[firstName]." ".$row[lastName]."<br>";
-	}
-	echo "</td><td>";
-	$query = $db->query("SELECT firstName, lastName, memberPoints FROM Member WHERE memFamilyID=6 AND status != 'alumni' ORDER BY memberPoints DESC, lastName, firstName");
-		$query->setFetchMode(PDO::FETCH_ASSOC);
-	while($row = $query->fetch()) {
-		echo $row[memberPoints]."<br>";
-	}
-	echo "</td></tr>";
-	
 ?>
 </table>
 	
