0a7993cbaf4185b83702a5da4040fb2b2fb8e698
Akshay Easwaran <akeaswaran@me.com>
2019-03-02 16:21:15 -0500

    Updated CSS for rankings page

--- rankings.php
+++ rankings.php
@@ -1,17 +1,15 @@
 <?php
-	require "logged_in_check.php";
-	require "database_connect.php";
-	
-	require "html_header_begin.txt";
+require "logged_in_check.php";
+require "set_session_vars_full.php";
+require "database_connect.php";
+$pageTitle = "Members";
 ?>
-<SCRIPT language=JavaScript>
-function reload(form){
-var sortBy=form.sortBy.options[form.sortBy.options.selectedIndex].value;
-self.location='rankings.php?sortBy=' + sortBy;
-}
-</SCRIPT>
-<?php require "html_header_end.txt"; ?>
-<br/>
+
+<!DOCTYPE html>
+<html>
+<?php require "partials/head.php"; ?>
+<body>
+<?php require "partials/header.php"; ?>
 <?php
 	@$sortBy=$_GET[sortBy];
 	
@@ -20,63 +18,84 @@ self.location='rankings.php?sortBy=' + sortBy;
 	    $sortBy = "all";
 	}
 ?>
-<h3>Complete Individual Rankings</h3>
+<div class="container">
+<div class="row mb-1">
+    <div class="col-10">
+        <h3>Complete Individual Rankings</h3>
+    </div>
+    <div class="col-2">
+        <form>
+            <select class="form-control float-right" name="sortBy" id="sortBy" onChange="reload(this.form)">
+                <option value="all"  <?PHP if($sortBy=="all") echo "selected";?>>All</option>
+                <option value="members"  <?PHP if($sortBy=="members") echo "selected";?>>Members</option>
+                <option value="probates"  <?PHP if($sortBy=="probates") echo "selected";?>>Probates</option>
+                <option value="social"  <?PHP if($sortBy=="social") echo "selected";?>>Social</option>
+            </select>
+        </form>
+    </div>
+</div>
+<div class="row mb-3">
+    <div class="col-12">
+        <a href="/stats.php" class="float-right">More fun stats</a>
+    </div>
+</div>
+<div class="row mb-3">
+    <div class="col-12">
+        <table class="table table-hover mb-3">
+            <thead>
+                <tr>
+                    <th scope="col">Rank</th>
+                    <th scope="col">Member</th>
+                    <td scope="col" align="right"><b>Total Points</b></td>
+                </tr>
+            </thead>
+            <tbody>
+            <?php
 
-<div align="center">
-<a href="points.php">Back to Points</a><br/>
-<a href="stats.php">More fun stats</a>
-</div><br/>
-<form>
-<table align="center">
-<tr><td>Sort by: </td>
-<td>
-<select name="sortBy" id="sortBy" onChange="reload(this.form)">
-   <option value="all"  <?PHP if($sortBy=="all") echo "selected";?>>All</option>
-   <option value="members"  <?PHP if($sortBy=="members") echo "selected";?>>Members</option>
-   <option value="probates"  <?PHP if($sortBy=="probates") echo "selected";?>>Probates</option>
-   <option value="social"  <?PHP if($sortBy=="social") echo "selected";?>>Social</option>
-</select>
-</td></tr>
-</table>
-</form>
-</br></br>
-<?php
+                // CREATE TABLE OF INDIVIDUAL RANKINGS
+                //---------------------------------------------------
 
-	// CREATE TABLE OF INDIVIDUAL RANKINGS
-	//---------------------------------------------------
+                if($sortBy=="all") {
+                    $query = $db->query("SELECT firstName, lastName, memberPoints, memberID FROM Member WHERE status!='alumni' ORDER BY memberPoints DESC, lastName");
+                        $query->setFetchMode(PDO::FETCH_ASSOC);
+                } elseif($sortBy=="members") {
+                    $query = $db->query("SELECT firstName, lastName, memberPoints, memberID FROM Member WHERE status='member' ORDER BY memberPoints DESC, lastName");
+                        $query->setFetchMode(PDO::FETCH_ASSOC);
+                } elseif($sortBy=="probates") {
+                    $query = $db->query("SELECT firstName, lastName, memberPoints, memberID FROM Member WHERE status='probate' ORDER BY memberPoints DESC, lastName");
+                        $query->setFetchMode(PDO::FETCH_ASSOC);
+                } elseif($sortBy=="social") {
+                    $query = $db->query("SELECT firstName, lastName, memberPoints, memberID FROM Member WHERE status='social' ORDER BY memberPoints DESC, lastName");
+                        $query->setFetchMode(PDO::FETCH_ASSOC);
+                }
 
-	echo "<table align=\"center\">";
-	echo "<tr bgcolor=\"#b3a369\"><th>Rank</th><th>Member</th><th width=100>Points</th><th>Check Events</th></tr>";
+                $count = 1;
 
-        if($sortBy=="all") {
-		   	$query = $db->query("SELECT firstName, lastName, memberPoints, memberID FROM Member WHERE status!='alumni' ORDER BY memberPoints DESC, lastName");
-				$query->setFetchMode(PDO::FETCH_ASSOC);
-        } elseif($sortBy=="members") {
-		   	$query = $db->query("SELECT firstName, lastName, memberPoints, memberID FROM Member WHERE status='member' ORDER BY memberPoints DESC, lastName");
-				$query->setFetchMode(PDO::FETCH_ASSOC);
-        } elseif($sortBy=="probates") {
-		   	$query = $db->query("SELECT firstName, lastName, memberPoints, memberID FROM Member WHERE status='probate' ORDER BY memberPoints DESC, lastName");
-				$query->setFetchMode(PDO::FETCH_ASSOC);
-        } elseif($sortBy=="social") {
-		   	$query = $db->query("SELECT firstName, lastName, memberPoints, memberID FROM Member WHERE status='social' ORDER BY memberPoints DESC, lastName");
-				$query->setFetchMode(PDO::FETCH_ASSOC);
-        }
-	
-	$count = 1;
+                while($row = $query->fetch())
+                    {
+                    echo "<form action=\"allAttended.php\" method=\"POST\">";
+                    echo "<tr><th>".$count."</th>";
+                    echo "<td><input class='btn btn-link p-0' type=\"submit\" value=\"".$row[firstName]." ".$row[lastName]."\"></td>";
+                    echo "<td align=\"right\">".$row[memberPoints]."</td>";
+//                    echo "<td align=\"right\"><input class='btn btn-link' type=\"submit\" value=\"Check Events\"></td></tr>";
+                    echo "<input type=\"hidden\" name=\"memberID\" value=\"".$row[memberID]."\">";
+                    echo "</form>";
+                    $count++;
+                    }
+            ?>
+            </tbody>
+        </table>
+    </div>
+</div>
+</div>
+<?php require "partials/footer.php"; ?>
+<?php require "partials/scripts.php"; ?>
+<script>
+    function reload(form){
+        var sortBy=form.sortBy.options[form.sortBy.options.selectedIndex].value;
+        self.location='rankings.php?sortBy=' + sortBy;
+    }
+</script>
+</body>
 
-	while($row = $query->fetch())
-		{
-		echo "<form action=\"allAttended.php\" method=\"POST\">";
-		echo "<tr><td align=\"center\">".$count."</td>";
-		echo "<td width=200>".$row[firstName]." ".$row[lastName]."</td>";
-		echo "<td align=\"center\">".$row[memberPoints]."</td>";
-		echo "<td align=\"center\"><input type=\"submit\" value=\"Check Events\"></td></tr>";
-		echo "<input type=\"hidden\" name=\"memberID\" value=\"".$row[memberID]."\">";
-		echo "</form>";
-		$count++;
-		}
-
-	echo "</table>";
-?>
-<br/><br/><br/><br/><br/><br/>
-<?php require "html_footer.txt"; ?>
+</html>
