32f31066e78be03aab52e7c8cd98516cdaca4a9d
Akshay Easwaran <akeaswaran@me.com>
2019-04-03 13:00:38 -0400

    updated short links

---
+++ HWTR/index.php
@@ -0,0 +1,3 @@
+<h3>Redirecting...</h3>
+<meta http-equiv="refresh" content="2; url=https://orgsync.com/130896/events/2710327/occurrences/6514780">
+
--- ROIS/index.php
+++ ROIS/index.php
@@ -1,3 +1,3 @@
 <h3>Redirecting...</h3>
-<meta http-equiv="refresh" content="2; url=https://orgsync.com/130896/events/2710327/occurrences/6514780">
+<meta http-equiv="refresh" content="2; url=https://orgsync.com/130896/events/2704824/occurrences/6508092">
 
