fb3020809c10d8c16b34c796868cd3e629e62fc7
Ethan  Rosman <ethanrosman@gatech.edu>
2020-01-06 21:26:40 -0500

    Update Member Status
    Fixed member update on the member settings page. The page now inserts member status to the table

--- memberSettings.php
+++ memberSettings.php
@@ -13,7 +13,8 @@
 	if($_POST[city]=="") { $_POST[city] = NULL; }
 	if($_POST[state]=="") { $_POST[state] = NULL; }	
 	if($_POST[zipCode]=="") { $_POST[zipCode] = NULL; }
-	if($_POST[joinYear]=="") { $_POST[joinYear] = NULL; }
+    if($_POST[status]=="") { $_POST[status] = NULL; }
+    if($_POST[joinYear]=="") { $_POST[joinYear] = NULL; }
 	if($_POST[gradMonth]=="") { $_POST[gradMonth] = NULL; }
 	if($_POST[gradYear]=="") { $_POST[gradYear] = NULL; }
 	if($_POST[reckerPair]=="") { $_POST[reckerPair] = NULL; }
@@ -22,10 +23,11 @@
 //    echo $_POST["memberID"];
 
 	$query = $db->prepare("UPDATE Member SET firstName=:firstName, lastName=:lastName, email=:email, phone=:phone, twitter=:twitter, streetAddress=:streetAddress, 
-		city=:city, state=:state, zipCode=:zipCode, joinYear=:joinYear, gradMonth=:gradMonth, gradYear=:gradYear, reckerPair=:reckerPair WHERE memberID = :memberID");
+		city=:city, state=:state, zipCode=:zipCode, status=:status, joinYear=:joinYear, gradMonth=:gradMonth, gradYear=:gradYear, reckerPair=:reckerPair WHERE memberID = :memberID");
 	$query->execute(array('firstName'=>$_POST[firstName], 'lastName'=>$_POST[lastName], 'email'=>$_POST[email], 'phone'=>$_POST[phone], 'twitter'=>$_POST[twitter], 
-		'streetAddress'=>$_POST[streetAddress], 'city'=>$_POST[city], 'state'=>$_POST[state], 'zipCode'=>$_POST[zipCode], 'joinYear'=>$_POST[joinYear], 
-		'gradMonth'=>$_POST[gradMonth], 'gradYear'=>$_POST[gradYear], 'reckerPair'=>$_POST[reckerPair], 'memberID'=>$_POST["memberID"]));
+		'streetAddress'=>$_POST[streetAddress], 'city'=>$_POST[city], 'state'=>$_POST[state], 'zipCode'=>$_POST[zipCode], 'status'=>$_POST[status],
+        'joinYear'=>$_POST[joinYear], 'gradMonth'=>$_POST[gradMonth], 'gradYear'=>$_POST[gradYear],
+        'reckerPair'=>$_POST[reckerPair], 'memberID'=>$_POST["memberID"]));
 
 	$_SESSION[firstName] = $_POST[firstName];
 	$_SESSION[lastName] = $_POST[lastName];
@@ -36,6 +38,7 @@
 	$_SESSION[city] = $_POST[city];
 	$_SESSION[state] = $_POST[state];
 	$_SESSION[zipCode] = $_POST[zipCode];
+	$_SESSION[status] = $_POST[status];
 	$_SESSION[joinYear] = $_POST[joinYear];
 	$_SESSION[gradMonth] = $_POST[gradMonth];
 	$_SESSION[gradYear] = $_POST[gradYear];
