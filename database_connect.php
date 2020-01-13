1a00039fd71ef8d245f2c7adbb3fc5e5dcf670a8
Akshay Easwaran <akeaswaran@me.com>
2020-01-12 22:34:23 -0800

    replacing everything with env vars to replace later

--- database_connect.php
+++ database_connect.php
@@ -1,12 +1,12 @@
 <?php
-	$host = 'localhost';
-	$port = 3306;
-	$database = 'reck_club';
-	$username = 'reck';
-	$password = 'burdell';
-	
-	$dsn = "mysql:host=$host;port=$port;dbname=$database";
-	
-	$db = new PDO($dsn, $username, $password);
-	
+$host = '127.0.0.1';
+$port = getenv('DB_PORT');
+$database = 'reck_club';
+$username = getenv('DB_USER');
+$password = getenv('DB_PASSWORD');
+
+$dsn = "mysql:host=$host;port=$port;dbname=$database";
+
+$db = new PDO($dsn, $username, $password);
+
 ?>
