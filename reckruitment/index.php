1a89e943242bbaa1a815ff7bdc503cd3c9aff79c
Ethan  Rosman <ethanrosman@gatech.edu>
2020-01-21 23:11:08 -0500

    Change 'W' to lowercase
    i'm bad at formatting

--- reckruitment/index.php
+++ reckruitment/index.php
@@ -59,7 +59,7 @@
 <h4 class="mb-2" id="first-social">First Social (Mandatory)</h4>
     <p>Attending this social is your chance as an applicant to meet Reck Clubbers for the first time, get a feel for what Ramblin’ Reck Club is all about, ask any questions you may have about the Club or getting involved, and most importantly, sign up for an interview time. No need to stress about this event; be yourself and talk to anyone you do not already know! Dress code for this event is <strong>business casual</strong>, and will be hosted in the Edge Building Trophy Room (on the corner of Techwood and Bobby Dodd).</p>
     <ul>
-        <li><strong>First Social: Monday, February 3rd in the </strong><a href="https://google.com/maps/place/150+Bobby+Dodd+Way+NW,+Atlanta,+GA+30313/@33.7725579,-84.3955688,17z/data=!3m1!4b1!4m5!3m4!1s0x88f50463078b1861:0xd9b0d387d74cdb9b!8m2!3d33.7725579!4d-84.3933801" target="blank">Arthur B. Edge Intercollegiate Athletics Center</a> (We'll have greeters outside)</li>
+        <li><strong>First Social: Monday, February 3rd in the </strong><a href="https://google.com/maps/place/150+Bobby+Dodd+Way+NW,+Atlanta,+GA+30313/@33.7725579,-84.3955688,17z/data=!3m1!4b1!4m5!3m4!1s0x88f50463078b1861:0xd9b0d387d74cdb9b!8m2!3d33.7725579!4d-84.3933801" target="blank">Arthur B. Edge Intercollegiate Athletics Center</a> (we'll have greeters outside)</li>
     </ul>
     <p>There are two time slots for the first social: <strong>7:00pm-8:00pm and 8:30pm-9:30pm</strong>. Please note down your availability on the application. If you are not available during either of these times, please email <a href="mailto:membershipchair@reckclub.org">Kassie Lee</a>.</p>
 
