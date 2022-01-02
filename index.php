e46f4ca9bcefb7f8965ba077fffd7b02f6ce6e4c
Keshav <pittkesh99@gmail.com>
2022-01-01 19:52:24 -0500

    Updating the image size and css based on mobile vs website

--- index.php
+++ index.php
@@ -17,7 +17,10 @@
         </ol>
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img class="img-responsive d-block w-100" src="img/2021/club_before_game.jpg" alt="2021 T-night picture">
+                <picture>
+                    <source class="img-responsive d-block w-100" srcset="img/2021/club_before_game_v3.jpg" media="(min-width: 900px)" alt="Reck Club pic before Football game">
+                    <img class="img-responsive d-block w-100" src="img/2021/club_before_game.jpg" alt="Reck Club pic before Football game">
+                </picture>
                 <div class="gradient">
                 </div>
                 <div class="carousel-caption d-none d-md-block">
@@ -26,7 +29,10 @@
                 </div>
             </div>
             <div class="carousel-item">
-                <img class="img-responsive d-block w-100" src="/img/2021/merc_ride_out.jpg" alt="2021 Mercedes-Benz game ride-out">
+                <picture>
+                    <source class="img-responsive d-block w-100" srcset="/img/2021/merc_ride_out_v2.jpg" media="(min-width: 900px)" alt="2021 Mercedes-Benz game ride-out">
+                    <img class="img-responsive d-block w-100" src="/img/2021/merc_ride_out.jpg" alt="2021 Mercedes-Benz game ride-out">
+                </picture>
                 <div class="gradient">
                 </div>
                 <div class="carousel-caption d-none d-md-block">
@@ -35,7 +41,10 @@
                 </div>
             </div>
             <div class="carousel-item">
-                <img class="img-responsive d-block w-100" src="/img/promo/ReckEcoCommons.png" alt="2021 Reck at Eco Commons building for a Reck Event">
+                <picture>
+                    <source class="img-responsive d-block w-100" srcset="/img/promo/ReckEcoCommons_v2.png" media="(min-width: 900px)" alt="2021 Reck at Eco Commons building for a Reck Event">
+                    <img class="img-responsive d-block w-100" src="/img/promo/ReckEcoCommons.png" alt="2021 Reck at Eco Commons building for a Reck Event">
+                </picture>
                 <div class="gradient">
                 </div>
                 <div class="carousel-caption d-none d-md-block">
@@ -44,7 +53,10 @@
                 </div>
             </div>
             <div class="carousel-item">
-                <img class="img-responsive d-block w-100" src="/img/2021/merc_alma_mater.jpg" alt="2021 Alma mater on the Mercedes Benz field">
+                <picture>
+                    <source class="img-responsive d-block w-100" srcset="/img/2021/merc_alma_mater_v2.jpg" media="(min-width: 900px)" alt="2021 Alma mater on the Mercedes Benz field">
+                    <img class="img-responsive d-block w-100" src="/img/2021/merc_alma_mater.jpg" alt="2021 Alma mater on the Mercedes Benz field">
+                </picture>
                 <div class="gradient">
                 </div>
                 <div class="carousel-caption d-none d-md-block">
